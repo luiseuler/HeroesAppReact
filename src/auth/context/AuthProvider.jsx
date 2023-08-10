44f8aa811fe42fd0b63c97152fe5b26f77fc8721
Luis Alejandro Garcia Garcia <luisalejandrogarciax2@gmail.com>
2023-08-10 12:02:41 -0600

    Login de un usuario

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,15 +1,28 @@
 import { useReducer } from "react";
 import { authReducer, AuthContext } from "./";
+import { types } from "../types/";
 
 const initialState = {
     logged: false
 }
 
 export const AuthProvider = ({ children }) => {
-    const [state, dispath] = useReducer(authReducer, initialState);
+    const [authState, dispath] = useReducer(authReducer, initialState);
+
+
+    const login = (name = '') => {
+        const action = {
+            type: types.login,
+            payload: {
+                id: 123,
+                name: name
+            }
+        }
+        dispath(action);
+    }
 
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{...authState, login}}>
             {children}
         </AuthContext.Provider>
     );
--- src/shared/components/NavBar.jsx
+++ src/shared/components/NavBar.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context';
 
 
 export const Navbar = () => {
+    const { user } = useContext(AuthContext);
+
     const navigate = useNavigate();
 
     const onLogOut = () => {
@@ -39,7 +43,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        Luis
+                        {user?.name}
                     </span>
 
                     <button className="nav-item nav-link btn"
