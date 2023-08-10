705a6a50c02efd6228b77e588fc16730ab551ada
Luis Alejandro Garcia Garcia <luisalejandrogarciax2@gmail.com>
2023-08-10 00:30:43 -0600

    SearchComponent

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,4 +1,22 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks";
+
 export const SearchPage = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const query = location.search.slice(2);
+
+    const { searchText, onInputChange } = useForm({
+        searchText: ''
+    });
+
+
+    const onSearchSubmit = (event) => {
+        event.preventDefault();
+        navigate(`?=${searchText}`);
+    }
+
     return (
         <>
             <h1>Search</h1>
@@ -8,12 +26,14 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
 
-                    <form>
+                    <form onSubmit={onSearchSubmit}>
                         <input type="text"
                             placeholder="Search a hero"
                             className="form-control"
                             name="searchText"
-                            autoComplete="off" />
+                            autoComplete="off" 
+                            value={searchText}
+                            onInput={onInputChange}/>
 
                         <button className="btn btn-outline-primary mt-1">
                             Search
@@ -27,7 +47,7 @@ export const SearchPage = () => {
 
                     <div className="alert alert-primary">Search a Hero</div>
 
-                    <div className="alert alert-danger">No hero with <b>1234</b></div>
+                    <div className="alert alert-danger">No hero with <b>{query}</b></div>
                 </div>
             </div>
         </>
