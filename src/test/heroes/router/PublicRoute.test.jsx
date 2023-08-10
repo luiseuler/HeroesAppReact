1d5f54f6e7027915773ff3cbaf0804b50275f3a6
Luis Alejandro Garcia Garcia <luisalejandrogarciax2@gmail.com>
2023-08-10 13:37:46 -0600

    Pruebas en el PublicRoute - Parte 2

--- src/test/heroes/router/PublicRoute.test.jsx
+++ src/test/heroes/router/PublicRoute.test.jsx
@@ -1,6 +1,7 @@
 const { render, screen } = require("@testing-library/react");
 const { AuthContext } = require("../../../auth/context/");
-const { PublicRoute } = require("../../../router/PublicRoute");
+const { PublicRoute } = require("../../../router/");
+const { MemoryRouter, Routes, Route } = require("react-router-dom");
 
 describe('Pruebas en <PublicRoute />', () => {
     test('debe de mostrar el children si no está autenticado', () => {
@@ -17,5 +18,34 @@ describe('Pruebas en <PublicRoute />', () => {
         );
 
         expect(screen.getByText('Ruta Pública')).toBeTruthy();
+    });
+
+    test('debe de navegar si estálogeado', () => {
+        const contextValue = {
+            logged: true,
+            user: {
+                id: 123,
+                name: 'strider'
+            }
+        };
+
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/login']}>
+                    <Routes>
+                        <Route path="/login" element={
+                            <PublicRoute>
+                                <h1>Ruta Pública</h1>
+                            </PublicRoute>
+                        }>
+                        </Route>
+
+                        <Route path="/marvel" element={<h1>Ruta Marvel</h1>}></Route>
+                    </Routes>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect(screen.getByText('Ruta Marvel')).toBeTruthy();
     });
 });
