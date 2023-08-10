ef0fdabccc621d229805bcdc26b111718fd53a13
Luis Alejandro Garcia Garcia <luisalejandrogarciax2@gmail.com>
2023-08-09 22:56:57 -0600

    Tarjetas con la información del Héroe

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,13 +1,14 @@
 import { getHeroesByPublisher } from "../helpers";
+import { HeroCard } from "./HeroCard";
 
-export const HeroList = ({publisher}) => {
+export const HeroList = ({ publisher }) => {
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+        <diiv className="row row-cols-1 row-cols-md-3 g-3">
             {
-                heroes.map(hero => (<li key={hero.id}>{hero.superhero}</li>))
+                heroes.map(hero => (<HeroCard key={hero.superhero} {...hero} />))
             }
-        </ul>
+        </diiv>
     );
 }
