ed5fe150d0219e3e9e0a5c38adad7f8032362e5e
Luis Alejandro Garcia Garcia <luisalejandrogarciax2@gmail.com>
2023-08-10 13:52:22 -0600

    Pruebas en el PrivateRoute

---
+++ src/test/heroes/router/PrivateRoute.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { AuthContext } from "../../../auth";
+import { PrivateRoute } from "../../../router";
+
+describe('Pruebas en <PrivateRoute />', () => {
+    test('debe de mostrar el children si está autenticado', () => {
+        Storage.prototype.setItem = jest.fn();
+
+        const contextValue = {
+            logged: true,
+            user: {
+                id: 346,
+                name: 'Luisisrrin'
+            }
+        };
+
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/search?q=batman']}>
+                    <PrivateRoute>
+                        <h1>Ruta Privada</h1>
+                    </PrivateRoute>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect(screen.getByText('Ruta Privada')).toBeTruthy();
+        expect(localStorage.setItem).toHaveBeenCalledWith('lastPath', '/search?q=batman');
+    });
+});
